a733a519ad65d2312f1e7574231710f81cd798d6
niocio01 <zuber.nico@gmail.com>
2018-11-03 18:22:19 +0100

    Pin definition

---
+++ include/Pins.h
@@ -0,0 +1,64 @@
+#include <core_pins.h>
+
+///////////////////// MENU BUTTONS /////////////////////
+
+#define BTN_BK_UP_PIN       2
+#define BTN_BK_DWN_PIN      3
+#define BTN_MODE_PIN        4
+#define BTN_SAVE_PIN        5
+#define BTN_BACK_PIN        6
+#define BTN_SELECT_PIN      7
+
+
+///////////////////// ENCODER /////////////////////
+
+#define ENCODER_CLK_PIN     9
+#define ENCODER_DT_PIN      8
+
+
+///////////////////// PEDAL /////////////////////
+
+#define PEDAL_PIN           17
+
+
+
+///////////////////// RX, TX FOR MIDI /////////////////////
+
+#define MIDI_IN_PIN         0
+#define MIDI_OUT_PIN        1
+
+
+///////////////////// SWITCHES /////////////////////
+
+// TRACK PLAY, OVERDUB
+#define TRACK1_PLAY_PIN     37
+#define TRACK1_OVD_PIN      29
+#define TRACK2_PLAY_PIN     38
+#define TRACK2_OVD_PIN      28
+#define TRACK3_PLAY_PIN     39
+#define TRACK3_OVD_PIN      27
+#define TRACK4_PLAY_PIN     25
+#define TRACK4_OVD_PIN      26
+#define TRACK5_PLAY_PIN     13
+#define TRACK5_OVD_PIN      24
+
+// CUSTOM SWITCHES
+#define CSTM_SW1_PIN        30
+#define CSTM_SW2_PIN        31
+#define CSTM_SW3_PIN        32
+#define CSTM_SW4_PIN        35
+#define CSTM_SW5_PIN        34
+
+
+///////////////////// OTHER PINS /////////////////////
+
+// I2C MULTIPLEXER RESET
+#define I2C_MUX_RESET_PIN   10
+
+// LED DRIVER RESET
+#define LED_DRVR1_RESET_PIN 16
+#define LED_DRVR2_RESET_PIN 17
+
+// BUILT IN LED
+#define TEENSY_LED_PIN 13
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+#include <core_pins.h>
+#include <kinetis.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  GPIOC_PSOR = (1<<5); // set Pin 13 to on
+  //digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(200);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000); 
 }
